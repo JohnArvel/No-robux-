b6d8429ad3c62260ca359de3a57f28934c259074
JohnArvel <134151205+JohnArvel@users.noreply.github.com>
2023-06-09 13:17:15 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,18 +1,18 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore importfrom PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel, QListWidget, QLineEdit
  
 from instr import *
 from second_win import *
 
 class MainWin(QWidget):
-   def __init__(self):
+    def __init__(self):
        super().__init__()
        self.initUI()
        self.connects()
        self.set_appear()
        self.show()
               
-   def initUI(self):
+    def initUI(self):
        self.btn_next = QPushButton(txt_next, self)
        self.hello_text = QLabel(txt_hello)
        self.instruction = QLabel(txt_instruction)
@@ -22,14 +22,13 @@ class MainWin(QWidget):
        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
        self.setLayout(self.layout_line)
-  
-   def next_click(self):
-       self.tw = TestWin()
-       self.hide()
- 
-   def connects(self):
+   
+    def connects(self):
        self.btn_next.clicked.connect(self.next_click)
-   def set_appear(self):
+    def next_click(self):
+       self.hide()
+       self.tw = TestWin()
+    def set_appear(self):
        self.setWindowTitle(txt_title)
        self.resize(win_width, win_height)
        self.move(win_x, win_y)
