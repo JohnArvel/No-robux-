7615669c8f740b36ed6a4deae68bc93912aaea39
JohnArvel <134151205+JohnArvel@users.noreply.github.com>
2023-06-09 13:18:39 +0300

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -1,22 +1,21 @@
 #final_win.py
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QLabel, QApplication,QPushButton,QVBoxLayout,QHBoxLayout,QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel, QListWidget, QLineEdit
 from instr import *
-class TestWin(QtWidget):
+class FinWin(QWidget):
     def __init__(self):
-        super()__init__()
+        super().__init__()
         self.set_appear()
         self.initUI()
-        self.connects()
         self.show()
     def set_appear(self):
-        self.setWindowTitle(txt_finalwin)
+        self.setWindowTitle(txt_title)
         self.resize(win_width,win_height)
         self.move(win_x,win_y)
     def initUI(self):
         self.line = QVBoxLayout()
         self.txt_index = QLabel(txt_index)
         self.txt_workheart = QLabel(txt_workheart)
-        self.line.addWidget(self.txt_index,aligment=Qt.AlignCenter)
-        self.line.addWidget(self.txt_workheart,aligment=Qt.AlignCenter)
-        final_win.setLayout(self.line)
+        self.line.addWidget(self.txt_index, alignment = Qt.AlignCenter)
+        self.line.addWidget(self.txt_workheart, alignment = Qt.AlignCenter)
+        self.setLayout(self.line)
